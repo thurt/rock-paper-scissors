9e452faf0993051991ff920af196963b64e7b117
Taylor Hurt <taylor.a.hurt@gmail.com>
2019-04-05 12:31:46 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let wins = 0;
+let losses = 0;
+let ties = 0;
+
+// this function
+// 1. returns "rock"/"paper"/"scissors" based on a random integer
+function computersMove() {}
+
+// this function
+// 1. increments the wins/ties/losses depending on the result
+// 2. updates the number of wins/ties/losses displayed on the website
+function updateScore(result) {}
+
+// this function
+// 1. calls computersMove to get the computer's move
+// 2. determines whether the user has "won"/"tied"/"lost" based on computer's move
+// 2. calls updateScore and passes the result "won"/"tied"/"lost" as a parameter
+function onClickRock() {}
+
+function onClickPaper() {}
+
+function onClickScissors() {}
+
+document.getElementById("rock").onclick = onClickRock;
+document.getElementById("paper").onclick = onClickPaper;
+document.getElementById("scissors").onclick = onClickScissors;
