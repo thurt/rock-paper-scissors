b4d02195acb360e5204d679f4fdb9708dc0be7bc
Taylor Hurt <taylor.a.hurt@gmail.com>
2019-04-12 12:36:27 -0400

    updates

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ let ties = 0;
 
 // this function
 // 1. returns "rock"/"paper"/"scissors" based on a random integer
-function computersMove() {}
+function computersMove() {
+  let randInt = Math.floor(Math.random() * 3); // generates 0, 1, or 2
+  if (randInt === 0) {
+    return "rock";
+  }
+  if (randInt === 1) {
+    return "paper";
+  }
+  if (randInt === 2) {
+    return "scissors";
+  }
+}
 
 // this function
 // 1. increments the wins/ties/losses depending on the result
@@ -14,7 +25,8 @@ function updateScore(result) {}
 // this function
 // 1. calls computersMove to get the computer's move
 // 2. determines whether the user has "won"/"tied"/"lost" based on computer's move
-// 2. calls updateScore and passes the result "won"/"tied"/"lost" as a parameter
+// 3. calls updateScore and passes the result "won"/"tied"/"lost" as a parameter
+// 4. alerts the user of the computers move and the results
 function onClickRock() {}
 
 function onClickPaper() {}
